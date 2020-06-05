68a05b9e8c020b4dcf71edd247217d54eb4d72a5
Boub <freeboub@gmail.com>
2020-06-05 23:18:47 +0200

    [LinkedIn] : remove copyrighted logo

---
+++ app/src/main/java/com/boub/share/onfb/GoogleActivity.kt
@@ -0,0 +1,18 @@
+/*
+ * ----------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * <freeboub@gmail.org> wrote this file. As long as you retain this notice you
+ * can do whatever you want with this stuff. If we meet some day, and you think
+ * this stuff is worth it, you can buy me a beer in return Olivier Bouillet
+ * ----------------------------------------------------------------------------
+ */
+
+package com.boub.share.onfb
+
+import android.net.Uri
+
+class GoogleActivity : ShareActivity() {
+    override fun getShareLink( originalRequest : String ): String {
+        return "https://search.lilo.org/searchweb.php?q=" + Uri.encode(originalRequest)
+    }
+}
